44b25b99f949b1f9f62aad76796bb52957821ebb
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-09-12 17:38:29 -0500

    funcion dibujarGuion

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var lineToX = [797,585,762,762,762,797,727,797,727];
 var moveToY = [365,5,5,5,117,117,117,252,252];
 var lineToY = [365,365,5,54,252,189,189,324,324];
 
+let palabrasSecretas = ["COMPILADOR","JAVASCRIPT","CODIGO","PROGRAMACION","GITHUB"];
+let palabraSecreta = "hftgyu56";
+
 function trazarFigura(x1,x2,y1,y2){
     pincel.strokeStyle= "#0A3871"
     pincel.beginPath();
@@ -16,6 +19,26 @@ function trazarFigura(x1,x2,y1,y2){
     pincel.lineJoin = "round";
     pincel.stroke();
 }
+
+function dibujarGuion(){
+    
+    pincel.lineWidth = 6;
+    pincel.lineCap = "round";
+    pincel.lineJoin = "round";
+    pincel.strokeStyle= "#0A3871";
+    for(var i = 0;i < palabraSecreta.length;i++){
+        
+        pincel.moveTo((450)+(80*i),480);
+        pincel.lineTo((500)+(80*i),480);
+    }
+
+    pincel.stroke();
+    pincel.closePath();
+} 
+
+dibujarGuion();
+
+
 trazarFigura(moveToX[0],lineToX[0],moveToY[0],lineToY[0])
 
 for(var i = 1;i<= 8; i ++){
@@ -28,18 +51,14 @@ pincel.beginPath(); //Cabeza
 pincel.arc(762,85.5,31.5,0,2*3.14);
 pincel.stroke();
 
-function mostrarInput(n){
-    
-}
-
 texto = "Hola mundo";
 
 function dibujarTexto(texto,textoX,textoY){
 pincel.beginPath();
 pincel.strokeStyle= "blue";
-pincel.font = "bold 20px arial";
+pincel.font = "bold 40px arial";
 pincel.fillText(texto,textoX,textoY);
 }
 
-dibujarTexto(texto,600,400)
+dibujarTexto(texto,600,450)
 
