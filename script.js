3a827a5c7ce5336cac686373bbb9566cc89cbf41
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-09-13 17:30:23 -0500

    palabras arriba de guiones

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var lineToX = [797,585,762,762,762,797,727,797,727];
 var moveToY = [365,5,5,5,117,117,117,252,252];
 var lineToY = [365,365,5,54,252,189,189,324,324];
 
-let palabrasSecretas = ["COMPILADOR","JAVASCRIPT","CODIGO","PROGRAMACION","GITHUB"];
+let palabrasSecretas = ["COMPILAR","JAVASCRIPT","CODIGO","PROGRAMA","GITHUB","ALURA","ORACLE","ONE","AHORCADO","ESTUDIO","ASTROS","MEDUSAS","IRONMAN"];
 let palabraSecreta = "";
 
 function sorteoPalabra(){
@@ -14,7 +14,7 @@ function sorteoPalabra(){
     palabraSecreta = palabraAleatoria;
     console.log(palabraSecreta);
 }
-
+sorteoPalabra();
 
 function trazarFigura(x1,x2,y1,y2){
     pincel.strokeStyle= "#0A3871"
@@ -35,8 +35,8 @@ function dibujarGuion(){
     pincel.strokeStyle= "#0A3871";
     for(var i = 0;i < palabraSecreta.length;i++){
         
-        pincel.moveTo((450)+(80*i),480);
-        pincel.lineTo((500)+(80*i),480);
+        pincel.moveTo((350)+(80*i),480);
+        pincel.lineTo((400)+(80*i),480);
     }
 
     pincel.stroke();
@@ -59,14 +59,17 @@ pincel.beginPath();
 pincel.arc(762,85.5,31.5,0,2*3.14);
 pincel.stroke();
 
-texto = "H";
 
-function dibujarTexto(texto,textoX,textoY){
+
+function dibujarTexto(texto){
 pincel.beginPath();
 pincel.strokeStyle= "blue";
 pincel.font = "bold 40px arial";
-pincel.fillText(texto,textoX,textoY);
+for(var i = 0;i < palabraSecreta.length;i++){
+        pincel.fillText(texto[i],(365)+(80*i),470);
 }
 
-dibujarTexto(texto,600,450)
+}
+
+dibujarTexto(palabraSecreta);
 
